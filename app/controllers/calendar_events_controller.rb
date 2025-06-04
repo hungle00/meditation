062e3b59e0727d70f0b8d99ac0202bfc729a1eb8
hungle00 <hungle2048@gmail.com>
2025-06-04 16:24:13 +0700

    Add calendar events

--- app/controllers/calendar_events_controller.rb
+++ app/controllers/calendar_events_controller.rb
@@ -1,4 +1,32 @@
 class CalendarEventsController < ApplicationController
+  before_action :fetch_dates, only: [:index, :create]
+
   def index
+    @calendar_events = CalendarEvent.where(occurs_at: @date_range).order(:occurs_at).group_by(&:occurs_at_date)
+    @calendar_event = CalendarEvent.new
   end
+
+  def create
+    @calendar_event = CalendarEvent.new(calendar_event_params)
+    @calendar_event.save!
+    redirect_to calendar_events_path
+  end
+
+  private
+
+    def calendar_event_params
+      params.require(:calendar_event).permit(:occurs_at, :description, :color)
+    end
+
+    def fetch_dates
+      @start_date = start_date.to_date
+      @date_range = (@start_date..@start_date.end_of_month)
+      @dates = @date_range.to_a
+      @dates.prepend(@dates.first - 1) until @dates.first.sunday?
+      @dates.append(@dates.last + 1) until @dates.last.saturday?
+    end
+
+    def start_date
+      params[:start_date] || Date.current.beginning_of_month
+    end
 end
--- app/helpers/calendar_events_helper.rb
+++ app/helpers/calendar_events_helper.rb
@@ -1,2 +1,11 @@
 module CalendarEventsHelper
+  def color_code(color)
+   case color
+    when 'green' then 'text-green-500'
+    when 'yellow' then 'text-yellow-500'
+    when 'red' then 'text-red-500'
+    else
+      'text-indigo-500'
+    end
+  end
 end
